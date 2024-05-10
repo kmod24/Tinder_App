affea1e5eeb48a033f625ecbb8e29e385a052323
Kyle Modina <116676173+kmod24@users.noreply.github.com>
2024-05-09 19:48:33 -0700

    Create Match.swift

---
+++ Tinder_App/Match.swift
@@ -0,0 +1,20 @@
+//
+//  Match.swift
+//  Tinder_App
+//
+//  Created by Kyle Modina on 5/6/24.
+//
+
+import Foundation
+
+struct Match {
+    let name: String
+    let profileImageUrl: String
+    let uid: String
+    
+    init(dictionary: [String: Any]) {
+        self.name = dictionary["name"] as? String ?? ""
+        self.profileImageUrl = dictionary["profileImageUrl"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+    }
+}
