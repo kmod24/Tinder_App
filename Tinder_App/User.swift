e006504f730437a4cc961fabc44fabe357d2898c
Kyle Modina <116676173+kmod24@users.noreply.github.com>
2024-05-09 19:48:16 -0700

    Create User.swift
    sets the parameters for the user, connects it to the database

---
+++ Tinder_App/User.swift
@@ -0,0 +1,36 @@
+//
+//  User.swift
+//  Tinder_App
+//
+//  Created by Kyle Modina on 4/1/24.
+//
+
+import UIKit
+
+
+//this sets the parameters for the user 
+
+struct User {
+    var name: String
+    var age: Int
+    var email: String
+    let uid: String
+    var imageURLs: [String]
+    var profession: String
+    var minSeekingAge: Int
+    var maxSeekingAge: Int
+    var bio: String
+    
+    
+    init(dictionary: [String: Any]) {
+        self.name = dictionary["fullname"] as? String ?? ""
+        self.age = dictionary["age"] as? Int ?? 0
+        self.email = dictionary["email"] as? String ?? ""
+        self.imageURLs = dictionary["imageURLs"] as? [String] ?? [String]()
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.profession = dictionary["profession"] as? String ?? ""
+        self.minSeekingAge = dictionary["minSeekingAge"] as? Int ?? 18
+        self.maxSeekingAge = dictionary["maxSeekingAge"] as? Int ?? 40
+        self.bio = dictionary["bio"] as? String ?? ""
+    }
+}
